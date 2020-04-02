634dfe75793fe6d0fd96c57d3a5408e26d837a2f
Matt McManus <matt@mcmanus.io>
2020-04-01 22:01:57 -0400

    Move resource loading into data service

--- app/routes/resources.js
+++ app/routes/resources.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class ResourcesRoute extends Route {
+export default class ResourceRoute extends Route {
+  @service data;
+
+  async beforeModel() {
+    return this.data.loadAllTask.perform();
+  }
 }
