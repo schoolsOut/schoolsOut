5967ef1d9d14cec2f8b793c08ed21df0aed85603
Max Minkoff <max@planetminkoff.com>
2020-08-12 15:57:32 -0400

    Installed and configured ember-metrics for Google Analytics.

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,6 +3,20 @@ import { inject as service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
   @service data;
+  @service metrics;
+  @service router
+
+  constructor() {
+    super(...arguments);
+
+    let router = this.router;
+    router.on('routeDidChange', () => {
+      const page = router.currentURL;
+      const title = router.currentRouteName || 'unknown';
+
+      this.metrics.trackPage({ page, title });
+    });
+  }
 
   async beforeModel() {
     return this.data.loadSettingsTask.perform();
